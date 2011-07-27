ebc88f5b383d698e26ef4f298adf6917e3a4408d
Michael McGee <michael@pantaloons.co.nz>
2011-07-27 17:40:51 +1200

    initial mesh implementations

--- mesh.c
+++ mesh.c
@@ -1,65 +1,22 @@
-#include <GL/gl.h>
-#include <GL/glu.h>
-#include <GL/glut.h>
+#include "mesh.h"
 
-#define WINDOW_START_WIDTH 640
-#define WINDOW_START_HEIGHT 480
-
-int width = WINDOW_START_WIDTH;
-int height = WINDOW_START_HEIGHT;
-
-void glInit() {
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LESS);
-	
-	glShadeModel(GL_SMOOTH);
-	
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
-
-	glEnable(GL_AUTO_NORMAL);
-	glEnable(GL_NORMALIZE);
-	glClearColor(0.5, 0.5, 0.5, 1.0);
+Mesh* initMesh(int numVertices, int numFaces, int numEdges, Vertex** verts, Face** faces, Edge** edges) {
+	Mesh* m = malloc(sizeof(Mesh));
+	m->numVertices = numVertices;
+	m->numFaces = numFaces;
+	m->numEdges = numEdges;
+	m->verts = verts;
+	m->faces = faces;
+	m->edges = edges;
+	return m;
 }
 
-void reshape(GLint newWidth, GLint newHeight) {
-	width = newWidth;
-	height = newHeight;
-	glViewport(0, 0, width, height);
-	
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(70.0f, width / (float)height, 1.0f, 100.0f);
-	
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(-25, -25, -25, 0, 0, 0, 0, 1, 0);
-}
-
-void draw() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	
-	render();
-	
-	glutSwapBuffers();
-}
-
-void render() {
-}
-
-int main(int argc, char** argv) {
-	glutInit(&argc, argv);
-	glutInitWindowSize(width, height);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
-	glutCreateWindow("Mesh simplification");
-	
-	glInit();
-	
-	glutReshapeFunc(reshape);
-	glutDisplayFunc(draw);
-	
-	
-	glutMainLoop();
-	
-	return 0;
+void destroyMesh(Mesh* m) {
+	for(int i = 0; i < m->numEdges; i++) free(m->edges[i]);
+	for(int i = 0; i < m->numVertices; i++) free(m->verts[i]);
+	for(int i = 0; i < m->numFaces; i++) free(m->faces[i]);
+	free(m->edges);
+	free(m->verts);
+	free(m->faces);
+	free(m);
 }
